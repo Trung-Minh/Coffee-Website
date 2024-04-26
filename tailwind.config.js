209b5b9c20aec3fc1a1219310efa383ff1fcd6d0
Trung goodboy <trungleminh76@gmail.com>
2024-04-26 22:44:40 +0700

    End header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./public/**/*.{html,js}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                "Karla": ['Karla', 'sans-serif']
+            },
+            colors: {
+                "light-coffee": "#c89f94",
+            },
+            keyframes: {
+                slideDown: {
+                    '0%': { transform: 'transformY(-100%)' },
+                    '100%': { transform: 'transformY(0)' },
+                }
+            },
+            animation: {
+                slideDown: 'slideDown .4s ease-in-out'
+            }
+        },
+    },
+    plugins: [],
 };
